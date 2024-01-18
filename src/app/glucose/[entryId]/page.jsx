b8c930efdc836a5936f31fbda2b6acdc6d365294
Sushant Pawar <sushpawar001@gmail.com>
2024-01-18 21:18:35 +0530

    Possible fix of date offset

--- src/app/glucose/[entryId]/page.jsx
+++ src/app/glucose/[entryId]/page.jsx
@@ -28,6 +28,7 @@ export default function EditEntry({ params }) {
     const submitForm = async (e) => {
         e.preventDefault()
         try {
+            data.createdAt = new Date(data.createdAt).toISOString()
             const response = await axios.put(`/api/glucose/update/${params.entryId}`, data)
             console.log(response);
             notify(response.data.message, 'success')
