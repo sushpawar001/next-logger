178efe929f4d6a341ad36605c40d95646fe2f585
Sushant Pawar <sushpawar001@gmail.com>
2024-06-14 21:54:50 +0530

    Middleware and CORS update

--- src/middleware.js
+++ src/middleware.js
@@ -1,38 +1,42 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const path = request.nextUrl.pathname;
-  const allowed = ["/load"];
-  const isPublicPath = path === "/login" || path === "/signup";
+    const path = request.nextUrl.pathname;
+    const allowed = ["/load"];
+    const isPublicPath = path === "/login" || path === "/signup";
 
-  const token = request.cookies.get("token")?.value || "";
+    const token = request.cookies.get("token")?.value || "";
+    console.log("isPublicPath", path, isPublicPath);
 
-  if (path in allowed) {
-    return;
-  }
+    if (allowed.includes(path)) {
+        // allow public paths
+        return;
+    }
 
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL("/", request.nextUrl));
-  }
+    if (isPublicPath && token) {
+      // if user is logged in, redirect to home
+        return NextResponse.redirect(new URL("/", request.nextUrl));
+    }
 
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl));
-  }
+    if (!isPublicPath && !token) {
+      // if user is not logged in, redirect to login
+        return NextResponse.redirect(new URL("/login", request.nextUrl));
+    }
 }
 
 // See "Matching Paths" below to learn more
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
-    "/((?!api|_next/static|_next/image|favicon.ico).*)",
-    "/login",
-    "/signup",
-  ],
+    matcher: [
+        /*
+         * Match all request paths except for the ones starting with:
+         * - api (API routes)
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         */
+        "/((?!api|_next/static|_next/image|favicon.ico).*)",
+        "/login",
+        "/signup",
+    ],
 };
