10c5a69e402b53dca3971bf59526da110cf05cad
Sushant Pawar <sushpawar001@gmail.com>
2025-05-18 13:36:56 +0530

    Refactor date formatting functions for consistency and improved timezone handling

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,18 +1,20 @@
-import moment from 'moment-timezone';
+import moment from "moment-timezone";
 
 export function DatetimeLocalFormat(rawDate) {
-    return moment(rawDate).format('YYYY-MM-DDTHH:mm')
+    return moment(rawDate).format("YYYY-MM-DDTHH:mm");
 }
 
 export function ShortDateformat(rawDate) {
-    const formattedDate = moment.utc(rawDate)
-        .tz('Asia/Kolkata')
-        .format('DD-MM hh:mm A');
-    return formattedDate
+    const formattedDate = moment
+        .utc(rawDate)
+        .tz(moment.tz.guess())
+        .format("DD-MM hh:mm A");
+    return formattedDate;
 }
 export default function formatDate(rawDate) {
-    const formattedDate = moment.utc(rawDate)
-        .tz('Asia/Kolkata')
-        .format('DD MMM YY, hh:mm A');
-    return formattedDate
+    const formattedDate = moment
+        .utc(rawDate)
+        .tz(moment.tz.guess())
+        .format("DD MMM YY, hh:mm A");
+    return formattedDate;
 }
