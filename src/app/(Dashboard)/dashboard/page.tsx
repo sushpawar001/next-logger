9b1b4da61c0741a18fab983811177d4fee82eb93
Sushant Pawar <sushpawar001@gmail.com>
2025-06-26 23:30:20 +0530

    refactor: remove unused imports and simplify Dashboard component to focus on DiabetesDashboard

--- src/app/(Dashboard)/dashboard/page.tsx
+++ src/app/(Dashboard)/dashboard/page.tsx
@@ -1,9 +1,4 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import FitnessDashboard from "@/components/Dashboards/FitnessDashboard";
 import DiabetesDashboard from "@/components/Dashboards/DiabetesDashboard";
-import { getDashboardLayout } from "@/helpers/getDashboardLayout";
 
 export default function Dashboard() {
     return <DiabetesDashboard />;
